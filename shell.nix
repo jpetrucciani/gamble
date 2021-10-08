d514495b7d1c9944b5a6f5e1a3aeb0aa87f59e4c
Jacobi Petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2021-10-07 20:34:21 -0400

    add python 3.10 to testing, update shell.nix (#13)

--- shell.nix
+++ shell.nix
@@ -1,10 +1,22 @@
 let
+  nixpkgs = import
+    (
+      fetchTarball {
+        name = "nixos-unstable-2021-08-31";
+        url = "https://github.com/NixOS/nixpkgs/archive/cb021898fab2a19e75d4e10c10c1da69c4e9f331.tar.gz";
+        sha256 = "1hxpp44bg1gwfzcd570wqfvd6am4vk52938zqcwy7mxwjmk6wbrh";
+      }
+    )
+    { };
   mach-nix = import
     (builtins.fetchGit {
       url = "https://github.com/DavHau/mach-nix/";
       ref = "refs/tags/3.3.0";
     })
-    { python = "python3"; };
+    {
+      pkgs = nixpkgs;
+      python = "python39";
+    };
 in
 mach-nix.mkPythonShell {
   requirements = builtins.readFile ./requirements.dev.txt;
